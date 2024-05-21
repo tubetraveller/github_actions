5fbb148a01ab96c4e7b3fdad3864560345e4cad5
Max <daniel@datascientest.com>
2024-05-21 09:20:07 +0000

    Bugfixes file_test.py

--- file_test.py
+++ file_test.py
@@ -1,21 +1,25 @@
-#import pytest
+import pytest
 
-def  test_calc_addition():
-  # Function test the output of 2+4
-    output = 2+4
+def test_calc_addition():
+    # Function tests the output of 2+4
+    output = 2 + 4
     assert output == 6
 
-def  test_calc_substraction():
-  # Function test the output of 2-4
-    output = 2-4
+
+def test_calc_substraction():
+    # Function tests the output of 2-4
+    output = 2 - 4
     assert output == -2
 
-def  test_calc_multiply():
-  # Function test the output of 2*4
-    output = 2*4
+
+def test_calc_multiply():
+    # Function tests the output of 2*4
+    output = 2 * 4
     assert output == 8
 
-def  test_coucou():
-  # Function test if the output return 'hello'
-    output='hello'
+
+def test_coucou():
+    # Function tests if the output returns 'hello'
+    output = 'hello'
     assert output == 'hello'
+
